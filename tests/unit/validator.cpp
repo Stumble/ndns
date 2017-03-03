7b74b09802843060f8853c05f2bc097732e104f4
Yumin Xia <me@yumin-xia.com>
2017-03-02 23:36:00 -0800

    restore validator unit test
    Change-Id: I2af9d759d365a4990aae868bebcf7432fd6147fc

--- tests/unit/validator.cpp
+++ tests/unit/validator.cpp
@@ -1,261 +1,220 @@
-// /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-// /**
-//  * Copyright (c) 2014-2017, Regents of the University of California.
-//  *
-//  * This file is part of NDNS (Named Data Networking Domain Name Service).
-//  * See AUTHORS.md for complete list of NDNS authors and contributors.
-//  *
-//  * NDNS is free software: you can redistribute it and/or modify it under the terms
-//  * of the GNU General Public License as published by the Free Software Foundation,
-//  * either version 3 of the License, or (at your option) any later version.
-//  *
-//  * NDNS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-//  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-//  * PURPOSE.  See the GNU General Public License for more details.
-//  *
-//  * You should have received a copy of the GNU General Public License along with
-//  * NDNS, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
-//  */
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2014-2017, Regents of the University of California.
+ *
+ * This file is part of NDNS (Named Data Networking Domain Name Service).
+ * See AUTHORS.md for complete list of NDNS authors and contributors.
+ *
+ * NDNS is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * NDNS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * NDNS, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-// #include "validator.hpp"
+#include "validator.hpp"
 
-// #include "test-common.hpp"
+#include "test-common.hpp"
+#include "util/cert-helper.hpp"
 
-// namespace ndn {
-// namespace ndns {
-// namespace tests {
+#include <ndn-cxx/util/io.hpp>
 
-// NDNS_LOG_INIT("ValidatorTest")
+namespace ndn {
+namespace ndns {
+namespace tests {
 
-// BOOST_AUTO_TEST_SUITE(Validator)
+NDNS_LOG_INIT("ValidatorTest")
 
-// class Fixture : public IdentityManagementFixture
-// {
-// public:
-//   Fixture()
-//     : m_testId1("/test02")
-//     , m_testId2("/test02/ndn")
-//     , m_testId3("/test02/ndn/edu")
-//     , m_randomId("/test03")
-//     , m_version(name::Component::fromVersion(0))
-//     , m_face(m_keyChain, {false, true})
-//   {
-//     m_randomDsk = createRoot(m_randomId); // generate a root cert
+BOOST_AUTO_TEST_SUITE(Validator)
 
-//     m_dsk1 = createRoot(m_testId1); // replace to root cert
-//     m_dsk2 = createIdentity(m_testId2, m_dsk1);
-//     m_dsk3 = createIdentity(m_testId3, m_dsk2);
+class Fixture : public IdentityManagementFixture
+{
+public:
+  Fixture()
+    : m_testId1("/test02")
+    , m_testId2("/test02/ndn")
+    , m_testId3("/test02/ndn/edu")
+    , m_randomId("/test03")
+    , m_face(m_keyChain, {false, true})
+  {
+    m_randomDsk = createRoot(Name(m_randomId).append("NDNS")); // generate a root cert
 
-//     m_selfSignCert = m_keyChain.generateRsaKeyPair(m_testId3, false);
-//     shared_ptr<IdentityCertificate> cert = m_keyChain.selfSign(m_selfSignCert);
-//     m_selfSignCert = cert->getName();
-//     m_keyChain.addCertificate(*cert);
-//     NDNS_LOG_TRACE("add cert: " << cert->getName() << " to KeyChain");
+    m_dsk1 = createRoot(Name(m_testId1).append("NDNS")); // replace to root cert
+    m_dsk2 = createIdentity(Name(m_testId2).append("NDNS"), m_dsk1);
+    m_dsk3 = createIdentity(Name(m_testId3).append("NDNS"), m_dsk2);
 
-//     m_face.onSendInterest.connect(bind(&Fixture::respondInterest, this, _1));
-//   }
+    m_face.onSendInterest.connect(bind(&Fixture::respondInterest, this, _1));
+  }
 
-//   ~Fixture()
-//   {
-//     m_face.getIoService().stop();
-//     m_face.shutdown();
-//   }
+  ~Fixture()
+  {
+    m_face.getIoService().stop();
+    m_face.shutdown();
+  }
 
-//   const Name
-//   createIdentity(const Name& id, const Name& parentCertName)
-//   {
-//     Name kskCertName = m_keyChain.createIdentity(id);
-//     Name kskName = m_keyChain.getDefaultKeyNameForIdentity(id);
-//     m_keyChain.deleteCertificate(kskCertName);
-//     auto kskCert = createCertificate(kskName, parentCertName);
+  const Key
+  createIdentity(const Name& id, const Key& parentKey)
+  {
+    Identity identity = addIdentity(id);
+    Key defaultKey = identity.getDefaultKey();
+    m_keyChain.deleteKey(identity, defaultKey);
 
-//     Name dskName = m_keyChain.generateRsaKeyPair(id, false);
-//     auto dskCert = createCertificate(dskName, kskCert);
-//     return dskCert;
-//   }
+    Key ksk = m_keyChain.createKey(identity);
+    Name defaultKskCert = ksk.getDefaultCertificate().getName();
+    m_keyChain.deleteCertificate(ksk, defaultKskCert);
 
-//   const Name
-//   createRoot(const Name& root)
-//   {
-//     m_rootCert = m_keyChain.createIdentity(root);
-//     ndn::io::save(*(m_keyChain.getCertificate(m_rootCert)), TEST_CONFIG_PATH "/anchors/root.cert");
-//     NDNS_LOG_TRACE("save root cert "<< m_rootCert <<
-//                   " to: " << TEST_CONFIG_PATH "/anchors/root.cert");
-//     Name dsk = m_keyChain.generateRsaKeyPair(root, false);
-//     auto cert = createCertificate(dsk, m_rootCert);
-//     return cert;
-//   }
+    Key dsk = m_keyChain.createKey(identity);
+    Name defaultDskCert = dsk.getDefaultCertificate().getName();
+    m_keyChain.deleteCertificate(dsk, defaultDskCert);
 
+    auto kskCert = createCertificate(m_keyChain, ksk, parentKey, "ID-CERT", time::days(100));
+    auto dskCert = createCertificate(m_keyChain, dsk, ksk, "ID-CERT", time::days(100));
 
-//   const Name
-//   createCertificate(const Name& keyName, const Name& parentCertName)
-//   {
-//     std::vector<CertificateSubjectDescription> desc;
-//     time::system_clock::TimePoint notBefore = time::system_clock::now();
-//     time::system_clock::TimePoint notAfter = notBefore + time::days(365);
-//     desc.push_back(CertificateSubjectDescription(oid::ATTRIBUTE_NAME,
-//                                                  "Signer: " + parentCertName.toUri()));
-//     shared_ptr<IdentityCertificate> cert =
-//       m_keyChain.prepareUnsignedIdentityCertificate(keyName, parentCertName,
-//                                                     notBefore, notAfter, desc);
+    m_keyChain.addCertificate(ksk, kskCert);
+    m_keyChain.addCertificate(dsk, dskCert);
 
-//     Name tmp = cert->getName().getPrefix(-1).append(m_version);
-//     cert->setName(tmp);
-//     m_keyChain.sign(*cert, parentCertName);
-//     m_keyChain.addCertificateAsKeyDefault(*cert);
-//     NDNS_LOG_TRACE("add cert: " << cert->getName() << " to KeyChain");
-//     return cert->getName();
-//   }
+    m_keyChain.setDefaultKey(identity, dsk);
+    return dsk;
+  }
 
+  const Key
+  createRoot(const Name& root)
+  {
+    Identity rootIdentity = addIdentity(root);
+    auto cert = rootIdentity.getDefaultKey().getDefaultCertificate();
+    ndn::io::save(cert, TEST_CONFIG_PATH "/anchors/root.cert");
+    NDNS_LOG_TRACE("save root cert "<< m_rootCert <<
+                  " to: " << TEST_CONFIG_PATH "/anchors/root.cert");
+    return rootIdentity.getDefaultKey();
+  }
 
-//   void
-//   respondInterest(const Interest& interest)
-//   {
-//     Name certName = interest.getName();
-//     if (certName.isPrefixOf(m_selfSignCert)) {
-//       // self-sign cert's version number is not m_version
-//       certName = m_selfSignCert;
-//     } else {
-//       certName.append(m_version);
-//     }
-//     NDNS_LOG_TRACE("validator needs: " << certName);
-//     BOOST_CHECK_EQUAL(m_keyChain.doesCertificateExist(certName), true);
-//     auto cert = m_keyChain.getCertificate(certName);
-//     m_face.getIoService().post([this, cert] {
-//         m_face.receive(*cert);
-//       });
-//   }
+  void
+  respondInterest(const Interest& interest)
+  {
+    Name keyName = interest.getName();
+    Name identityName = keyName.getPrefix(-2);
+    NDNS_LOG_TRACE("validator needs cert of KEY: " << keyName);
+    auto cert = m_keyChain.getPib().getIdentity(identityName)
+                                   .getKey(keyName)
+                                   .getDefaultCertificate();
+    m_face.getIoService().post([this, cert] {
+        m_face.receive(cert);
+      });
+  }
 
-// public:
-//   Name m_testId1;
-//   Name m_testId2;
-//   Name m_testId3;
-//   Name m_randomId;
+public:
+  Name m_testId1;
+  Name m_testId2;
+  Name m_testId3;
+  Name m_randomId;
 
-//   Name m_rootCert;
+  Name m_rootCert;
 
-//   Name m_dsk1;
-//   Name m_dsk2;
-//   Name m_dsk3;
+  Key m_dsk1;
+  Key m_dsk2;
+  Key m_dsk3;
 
-//   Name m_selfSignCert;
+  Key m_randomDsk;
 
-//   Name m_randomDsk;
-
-//   name::Component m_version;
-
-//   ndn::util::DummyClientFace m_face;
-// };
+  ndn::util::DummyClientFace m_face;
+};
 
 
-// BOOST_FIXTURE_TEST_CASE(Basic, Fixture)
-// {
-//   // validator must be created after root key is saved to the target
-//   ndns::Validator validator(m_face, TEST_CONFIG_PATH "/" "validator.conf");
+BOOST_FIXTURE_TEST_CASE(Basic, Fixture)
+{
+  // there is a precision issue in validity period
+  // temporarily using sleep to overcome it
+  sleep(1);
 
-//   Name dataName;
-//   dataName
-//     .append(m_testId3)
-//     .append("NDNS")
-//     .append("rrLabel")
-//     .append("rrType")
-//     .appendVersion();
-//   shared_ptr<Data> data = make_shared<Data>(dataName);
-//   m_keyChain.sign(*data, m_dsk3);
+  // validator must be created after root key is saved to the target
+  ndns::ValidatorNdns validator(m_face, TEST_CONFIG_PATH "/" "validator.conf");
 
-//   bool hasValidated = false;
-//   validator.validate(*data,
-//                      [&] (const shared_ptr<const Data>& data) {
-//                        hasValidated = true;
-//                        BOOST_CHECK(true);
-//                      },
-//                      [&] (const shared_ptr<const Data>& data, const std::string& str) {
-//                        hasValidated = true;
-//                        BOOST_CHECK(false);
-//                      });
+  // case1: record of testId3, signed by its dsk, should be successful validated.
+  Name dataName;
+  dataName
+    .append(m_testId3)
+    .append("NDNS")
+    .append("rrLabel")
+    .append("rrType")
+    .appendVersion();
+  shared_ptr<Data> data = make_shared<Data>(dataName);
+  m_keyChain.sign(*data, signingByKey(m_dsk3));
 
-//   m_face.processEvents(time::milliseconds(-1));
+  bool hasValidated = false;
+  validator.validate(*data,
+                     [&] (const Data& data) {
+                       hasValidated = true;
+                       BOOST_CHECK(true);
+                     },
+                     [&] (const Data& data, const ValidationError& str) {
+                       hasValidated = true;
+                       BOOST_CHECK(false);
+                     });
 
-//   BOOST_CHECK_EQUAL(hasValidated, true);
+  m_face.processEvents(time::milliseconds(-1));
 
-//   dataName = Name();
-//   dataName
-//     .append(m_testId2)
-//     .append("KEY")
-//     .append("rrLabel")
-//     .append("ID-CERT")
-//     .appendVersion();
-//   data = make_shared<Data>(dataName);
-//   m_keyChain.sign(*data, m_dsk3); // key's owner's name is longer than data owner's
+  BOOST_CHECK_EQUAL(hasValidated, true);
 
-//   hasValidated = false;
-//   validator.validate(*data,
-//                      [&] (const shared_ptr<const Data>& data) {
-//                        hasValidated = true;
-//                        BOOST_CHECK(false);
-//                      },
-//                      [&] (const shared_ptr<const Data>& data, const std::string& str) {
-//                        hasValidated = true;
-//                        BOOST_CHECK(true);
-//                      });
+  // case2: signing testId2's data by testId3's key, which should failed in validation
+  dataName = Name();
+  dataName
+    .append(m_testId2)
+    .append("NDNS")
+    .append("rrLabel")
+    .append("ID-CERT")
+    .appendVersion();
+  data = make_shared<Data>(dataName);
+  m_keyChain.sign(*data, signingByKey(m_dsk3)); // key's owner's name is longer than data owner's
 
-//   m_face.processEvents(time::milliseconds(-1));
-//   // cannot pass verification due to key's owner's name is longer than data owner's
-//   BOOST_CHECK_EQUAL(hasValidated, true);
+  hasValidated = false;
+  validator.validate(*data,
+                     [&] (const Data& data) {
+                       hasValidated = true;
+                       BOOST_CHECK(false);
+                     },
+                     [&] (const Data& data, const ValidationError& str) {
+                       hasValidated = true;
+                       BOOST_CHECK(true);
+                     });
 
-//   dataName = Name();
-//   dataName
-//     .append(m_testId3)
-//     .append("KEY")
-//     .append("rrLabel")
-//     .append("ID-CERT")
-//     .appendVersion();
-//   data = make_shared<Data>(dataName);
-//   m_keyChain.sign(*data, m_selfSignCert);
+  m_face.processEvents(time::milliseconds(-1));
+  // cannot pass verification due to key's owner's name is longer than data owner's
+  BOOST_CHECK_EQUAL(hasValidated, true);
 
-//   hasValidated = false;
-//   validator.validate(*data,
-//                      [&] (const shared_ptr<const Data>& data) {
-//                        hasValidated = true;
-//                        BOOST_CHECK(false);
-//                      },
-//                      [&] (const shared_ptr<const Data>& data, const std::string& str) {
-//                        hasValidated = true;
-//                        BOOST_CHECK(true);
-//                      });
+  // case4: totally wrong key to sign
+  dataName = Name();
+  dataName
+    .append(m_testId2)
+    .append("KEY")
+    .append("rrLabel")
+    .append("ID-CERT")
+    .appendVersion();
+  data = make_shared<Data>(dataName);
+  m_keyChain.sign(*data, signingByKey(m_randomDsk));
 
-//   m_face.processEvents(time::milliseconds(-1));
-//   // cannot pass due to self-sign cert is used
-//   BOOST_CHECK_EQUAL(hasValidated, true);
+  hasValidated = false;
+  validator.validate(*data,
+                     [&] (const Data& data) {
+                       hasValidated = true;
+                       BOOST_CHECK(false);
+                     },
+                     [&] (const Data& data, const ValidationError& str) {
+                       hasValidated = true;
+                       BOOST_CHECK(true);
+                     });
 
-//   dataName = Name();
-//   dataName
-//     .append(m_testId2)
-//     .append("KEY")
-//     .append("rrLabel")
-//     .append("ID-CERT")
-//     .appendVersion();
-//   data = make_shared<Data>(dataName);
-//   m_keyChain.sign(*data, m_randomDsk);
+  m_face.processEvents(time::milliseconds(-1));
+  // cannot pass due to a totally mismatched key
+  BOOST_CHECK_EQUAL(hasValidated, true);
+}
 
-//   hasValidated = false;
-//   validator.validate(*data,
-//                      [&] (const shared_ptr<const Data>& data) {
-//                        hasValidated = true;
-//                        BOOST_CHECK(false);
-//                      },
-//                      [&] (const shared_ptr<const Data>& data, const std::string& str) {
-//                        hasValidated = true;
-//                        BOOST_CHECK(true);
-//                      });
+BOOST_AUTO_TEST_SUITE_END()
 
-//   m_face.processEvents(time::milliseconds(-1));
-//   // cannot pass due to a totally mismatched key
-//   BOOST_CHECK_EQUAL(hasValidated, true);
-// }
-
-// BOOST_AUTO_TEST_SUITE_END()
-
-// } // namespace tests
-// } // namespace ndns
-// } // namespace ndn
+} // namespace tests
+} // namespace ndns
+} // namespace ndn
